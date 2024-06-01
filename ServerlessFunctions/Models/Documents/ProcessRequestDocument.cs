5b733f363df496a3d4a31706ce8a17f32fa2e649
Tim Cane <3074314+TimCane@users.noreply.github.com>
2024-06-01 22:58:06 +0100

    added code to check to see if the blend has already been processed.

---
+++ ServerlessFunctions/Models/Documents/ProcessRequestDocument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ServerlessFunctions.Models.Documents
+{
+    public class ProcessRequestDocument
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
